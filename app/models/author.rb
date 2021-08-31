b2e02147e7393bf920a150d315bd5c1ca94eb9bc
Zachary Goodman <znote89@gmail.com>
2021-08-31 10:43:52 -0400

    commit tree 436025c780f32641612d36bd868193b00f2a911c

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: {case_sensitive: true}
+    validates :phone_number, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait
+
+
+    def clickbait
+        if title == "True Facts"
+            errors.add(:title, "No clickbait allowed!")
+        end
+    end
 end
